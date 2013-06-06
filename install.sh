878bc9c2ce47c2d19399e1f4b53d3626d97bc657
Stefan Müller <co@zzeroo.com>
2013-06-06 18:56:32 +0200

    Change fonts dir and the install method to hardlinking

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-HBIN_FONT_DIR=$HOME/.font
+HBIN_FONT_DIR=$HOME/.fonts
 HBIN_FONTS=("Menlo-Regular.ttf"  "Monaco-Linux.ttf")
 
 function die () {
@@ -13,7 +13,11 @@ cat <<EOF
 --------------------------
  Top Programming Fonts!
 --------------------------
-  - Created and maintained by hbin
+  - Created by hbin
+
+  This install download and link (hardlink) the fonts.
+
+  shameless stolen by zzeroo!
 
 EOF
 
@@ -26,7 +30,7 @@ for i in ${HBIN_FONTS[*]}; do
     echo "Downloading $i"; echo
     wget -c https://github.com/hbin/top-programming-fonts/raw/master/$i \
       || die "Fail to download ${i}"
-    mv $i $HBIN_FONT_DIR || die "Could not install $i"
+    ln $i $HBIN_FONT_DIR || die "Could not install $i"
     echo "Installed $i successfully"; echo
 done
 
