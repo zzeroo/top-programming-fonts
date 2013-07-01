1cc9e51b431947b22600640960f1691838e260e8
-=..zZ[ER]Oo..=- <zzeroo@zzeroo.org>
2013-07-01 19:56:18 +0200

    Escape the powershell command

--- install.bat
+++ install.bat
@@ -14,7 +14,7 @@ FOR %%f IN %ZZ_FONTS% DO (
         IF "%1"=="" GOTO :EOF
         IF "%1"==" " GOTO :EOF
 	Echo.Downloading: %1 to %TEMP%\%1
-	powershell.exe -Command (new-object System.Net.WebClient).DownloadFile('https://raw.github.com/zzeroo/top-programming-fonts/master/%1','%TEMP%\\%1') >NUL
+	powershell.exe -Command "(new-object System.Net.WebClient).DownloadFile('https://raw.github.com/zzeroo/top-programming-fonts/master/%1','%TEMP%\\%1')" >NUL
 	ECHO Copy Font: %1 to %ZZ_FONTS_DIR%\%1
 	COPY /Y /B "%TEMP%\%1" "%USERPROFILE%\"
 EXIT /B
