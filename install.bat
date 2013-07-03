505cce93b21143bc5faf59daaa525303e8f12d7f
-=..zZ[ER]Oo..=- <zzeroo@zzeroo.org>
2013-07-03 09:28:28 +0200

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,3 +1,4 @@
+:: found here: https://github.com/zzeroo/top-programming-fonts
 @ECHO OFF
 SetLocal EnableDelayedExpansion
 CLS
@@ -9,13 +10,20 @@ FOR %%f IN %ZZ_FONTS% DO (
 	CALL :LOOP %%f
 )
 
+PAUSE
 
 :LOOP
+	::Download
         IF "%1"=="" GOTO :EOF
         IF "%1"==" " GOTO :EOF
 	Echo.Downloading: %1 to %TEMP%\%1
 	powershell.exe -Command "(new-object System.Net.WebClient).DownloadFile('https://raw.github.com/zzeroo/top-programming-fonts/master/%1','%TEMP%\\%1')" >NUL
 	
+	::Copy in
         ECHO Copy Font: %1 to %ZZ_FONTS_DIR%\%1
 	COPY /Y /B %TEMP%\%1 %ZZ_FONTS_DIR%\%1
+	
+	::Create regestry values
+	ECHO REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Fonts" /v %1:~0,-4% /t REG_SZ /d %1 /f 
+	REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Fonts" /v %1:~0,-4% /t REG_SZ /d %1 /f 
 EXIT /B
