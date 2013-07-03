4cf6ab1bed40eeb32bfbe5e6bb6db7932b393b36
-=..zZ[ER]Oo..=- <zzeroo@zzeroo.org>
2013-07-03 17:59:19 +0200

    Update install.bat

--- install.bat
+++ install.bat
@@ -6,7 +6,7 @@ CLS
 set ZZ_FONTS=("Menlo-Regular.ttf"  "Monaco.ttf")
 
 FOR %%f IN %ZZ_FONTS% DO (
-	CALL :LOOP %%f
+	CALL :FONT %%f
 )
 
 PAUSE
@@ -28,3 +28,18 @@ PAUSE
 	ECHO Add registry entry: ... 
 	REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Fonts" /v %FONT:~0,-5% (TrueType)" /t REG_SZ /d %FONT% /f 
 EXIT /B
+
+:FONT
+	SET FFILE=%~n1%~x1
+	SET FNAME=%~n1
+	SET FNAME=%FNAME:-= %
+	IF "%~x1"==".otf" SET FTYPE=(OpenType)
+	IF "%~x1"==".ttf" SET FTYPE=(TrueType)
+	
+	ECHO FILE=%FFILE%
+	ECHO NAME=%FNAME%
+	ECHO TYPE=%FTYPE%
+	
+	COPY /Y "%SRC%%~n1%~x1" "%SystemRoot%\Fonts\"
+	reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Fonts" /v "%FNAME% %FTYPE%" /t REG_SZ /d "%FFILE%" /f
+GOTO :EOF
