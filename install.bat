b6797c24c40f5e7adf10bf135c545c4b3a35dacd
-=..zZ[ER]Oo..=- <zzeroo@zzeroo.org>
2013-07-03 18:19:42 +0200

    Hopefully a working version....
    I hate windowz way to manage fonts. I hate registry, too.... NARF!

--- install.bat
+++ install.bat
@@ -11,24 +11,6 @@ FOR %%f IN %ZZ_FONTS% DO (
 
 PAUSE
 
-:LOOP
-	SET FONT=%1
-	
-	::Download
-        IF "%1"=="" GOTO :EOF
-        IF "%1"==" " GOTO :EOF
-	Echo.Downloading: %FONT% to %TEMP%\%FONT%
-	powershell.exe -Command "(new-object System.Net.WebClient).DownloadFile('https://raw.github.com/zzeroo/top-programming-fonts/master/%FONT%','%TEMP%\\%FONT%')" >NUL
-	
-	::Copy in
-        ECHO Copy %FONT% to %WINDIR%\FONTS\%FONT%
-	COPY /Y /B %TEMP%\%1 %WINDIR%\FONTS\%FONT%
-	
-	::Create regestry values
-	ECHO Add registry entry: ... 
-	REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Fonts" /v %FONT:~0,-5% (TrueType)" /t REG_SZ /d %FONT% /f 
-EXIT /B
-
 :FONT
 	SET FFILE=%~n1%~x1
 	SET FNAME=%~n1
@@ -36,10 +18,13 @@ EXIT /B
 	IF "%~x1"==".otf" SET FTYPE=(OpenType)
 	IF "%~x1"==".ttf" SET FTYPE=(TrueType)
 	
-	ECHO FILE=%FFILE%
+        ECHO FILE=%FFILE%
 	ECHO NAME=%FNAME%
 	ECHO TYPE=%FTYPE%
 	
-	COPY /Y "%SRC%%~n1%~x1" "%SystemRoot%\Fonts\"
+	::Copy in
+	ECHO COPY /Y %TEMP%\%FFILE% %WINDIR%\FONTS\
+	COPY /Y %TEMP%\%FFILE% %WINDIR%\FONTS\
+	
 	reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Fonts" /v "%FNAME% %FTYPE%" /t REG_SZ /d "%FFILE%" /f
 GOTO :EOF
